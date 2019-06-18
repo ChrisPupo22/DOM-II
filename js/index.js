a8ea15d1bb6933642ace7fdcdfa130c07db1adbe
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-06-18 17:21:36 -0400

    first push of the day, nav bar has been altered with event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+let colorNav = document.querySelector('nav');
+console.log(colorNav)
+
+colorNav.addEventListener('mouseover', event =>{
+    event.target.style.color = 'yellow'; 
+});
+
+colorNav.addEventListener('mouseout', event =>{
+    event.target.style.color = 'black';
+});
+
+let funBusAnim = document.querySelector('h1');
+console.log(funBusAnim);
+
+funBusAnim.addEventListener('select', event =>{
+    event.target.style.color = 'red';
+})
